26c1d7f24a58bb8a7fdef8d53fd0f49ca42bbdab
chongwyb <chongwyb@gmail.com>
2014-09-13 18:05:00 +0800

    New 2 unit test cases

--- CE2/src/TextBuddyTest.java
+++ CE2/src/TextBuddyTest.java
@@ -1,10 +1,19 @@
-import org.junit.Test;
+import static org.junit.Assert.*;
 
+import java.util.Stack;
+
+import org.junit.Test;
 
 public class TextBuddyTest {
 	@Test
-	public void testExecutedCommands(){
-		
-	}
+	public void testExecutedCommands() {
 
+		// Compare String
+		assertEquals("b", TextBuddy.initialSort(new Stack<String>(), "a", "b"));
+
+		// Get File Name
+		assertEquals("mytextfile",
+				TextBuddy.getFileName(new String[] { "mytextfile" }));
+
+	}
 }
